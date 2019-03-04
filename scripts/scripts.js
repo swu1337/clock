626df2bf63b86e4524ea208139ff748d10fd9682
swu1337 <master_msp1@hotmail.com>
2019-03-04 13:32:15 +0100

    clock added

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,26 @@
 window.onload = () => {
+    let clockHolder = document.querySelector('.clock');
+    
+    let showTime = () => {
+        let date = new Date();
+        let h = date.getHours();
+        let m = date.getMinutes();
+        let s = date.getSeconds();           
+
+        if(m < 10) {
+            m = `0${m}`;
+        }
+
+        if(s < 10) {
+            s = `0${s}`;
+        }
+
+        let stringTime = `${h}:${m}:${s}`;
+        clockHolder.innerHTML = stringTime;
+
+        setTimeout(showTime, 1000);
+    };
+
+    showTime();
 
 };
