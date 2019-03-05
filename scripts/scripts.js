5aff0202cd8e40277ad5cf471794b140150164c8
swu1337 <master_msp1@hotmail.com>
2019-03-05 09:32:42 +0100

    added list with timezones

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,26 +1,47 @@
 window.onload = () => {
-    let clockHolder = document.querySelector('.clock');
-    
+    const CLOCK_HOLDER = document.querySelector('.clock');
+    const SELECT = document.querySelector('.timezones');
+
+    fetch('https://raw.githubusercontent.com/dmfilipenko/timezones.json/master/timezones.json')
+        .then(res => res.json())
+        .then((options) => {
+            let htmlString = '';
+            
+            for(let option in options) {
+                let value = options[option];
+                let optionHTML = `<option value="${value.offset}">${value.text}</option>`;
+
+                if(value.abbr === 'WEDT') {
+                    optionHTML = `<option selected value="${value.offset}">${value.text}</option>`;
+                }
+
+                htmlString += optionHTML;
+            } 
+
+            SELECT.innerHTML = htmlString;
+        });
+
+    SELECT.onchange = () => {
+        console.log(SELECT.value);
+        let date = new Date();
+        let normalTime = date.getTime() + date.getTimezoneOffset() * 60000;
+        let newTime = normalTime * Number(SELECT.value);
+    }
+
     let showTime = () => {
         let date = new Date();
         let h = date.getHours();
         let m = date.getMinutes();
-        let s = date.getSeconds();           
+        let s = date.getSeconds();
 
-        if(m < 10) {
-            m = `0${m}`;
-        }
-
-        if(s < 10) {
-            s = `0${s}`;
-        }
+        m = m < 10 ? `0${m}` : m;
+        s = s < 10 ? `0${s}` : s;
 
         let stringTime = `${h}:${m}:${s}`;
-        clockHolder.innerHTML = stringTime;
+        CLOCK_HOLDER.innerHTML = stringTime;
 
         setTimeout(showTime, 1000);
     };
 
     showTime();
-
 };
